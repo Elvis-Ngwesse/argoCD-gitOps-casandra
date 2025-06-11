695f1a448c8617d497b9246c7cfa97c546ff1d31
Elvis Ngwesse <elvisngwesseaws@gmail.com>
2025-06-11 22:17:49 +0100

    synch changes

---
+++ unit-tests/test_app.py
@@ -0,0 +1,22 @@
+import pytest
+from app.main import app
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_index_status_code(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+def test_index_contains_keywords(client):
+    response = client.get('/')
+    data = response.data.decode()
+    assert "Customer Generator" in data
+    assert "Active and Running" in data
+    assert "Generating customers every" in data
